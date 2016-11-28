a3cd79ab207b596a6b639009353e57e3d41195d3
Gabriel Laskar <gabriel@lse.epita.fr>
2016-11-28 21:34:34 +0100

    mkkfs: please checkpatch
    Signed-off-by: Gabriel Laskar <gabriel@lse.epita.fr>

--- tools/mkkfs/mkkfs.c
+++ tools/mkkfs/mkkfs.c
@@ -1,26 +1,26 @@
 /*
-* Copyright (c) LSE
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in the
-*       documentation and/or other materials provided with the distribution.
-*
-* THIS SOFTWARE IS PROVIDED BY LSE AS IS AND ANY
-* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL LSE BE LIABLE FOR ANY
-* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Copyright (c) LSE
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY LSE AS IS AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL LSE BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #include <err.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -34,7 +34,7 @@
 
 #define kfswrite(Buf, Sz, S) \
 	do { \
-		if (!fwrite (Buf, Sz, 1, S)) \
+		if (!fwrite(Buf, Sz, 1, S)) \
 			err(1, "error writing to file\n"); \
 	} while (0)
 
@@ -50,8 +50,10 @@ static int verbose;
 static void *xcalloc(size_t nmemb, size_t sz)
 {
 	void *ptr = calloc(nmemb, sz);
+
 	if (!ptr)
 		err(1, "calloc failed");
+
 	return ptr;
 }
 
@@ -72,7 +74,8 @@ kfs_write_superblock(FILE *out, const char *fsname, uint32_t blk_cnt,
 	};
 
 	strncpy(sblock.name, fsname, KFS_NAME_SZ);
-	sblock.cksum = kfs_checksum(&sblock, sizeof(struct kfs_superblock) - sizeof(sblock.cksum));
+	sblock.cksum = kfs_checksum(&sblock,
+				    sizeof(sblock) - sizeof(sblock.cksum));
 
 	fseek(out, 0, SEEK_SET);
 	kfswrite(&sblock, sizeof(struct kfs_superblock), out);
@@ -85,8 +88,9 @@ kfs_write_superblock(FILE *out, const char *fsname, uint32_t blk_cnt,
  */
 static struct kfs_block *kfs_read_block(FILE *fp, struct kfs_block *blk)
 {
-	memset(blk, 0, sizeof(struct kfs_block));
-	if (!(blk->usage = fread(blk->data, 1, sizeof(blk->data), fp)))
+	memset(blk, 0, sizeof(*blk));
+	blk->usage = fread(blk->data, 1, sizeof(blk->data), fp);
+	if (!blk->usage)
 		return NULL;
 	return blk;
 }
@@ -105,6 +109,7 @@ static struct kfs_inode **kfs_alloc_inodes(char **files, size_t nb_files, uint32
 			sizeof(inodes[i]->filename));
 
 		struct stat st;
+
 		if (stat(files[i], &st) < 0)
 			err(1, "error stating file %s\n", files[i]);
 
@@ -127,7 +132,8 @@ static uint32_t
 kfs_write_inode(FILE *out, FILE *fp, struct kfs_inode *inode, uint32_t blk_idx)
 {
 	struct kfs_block blk;
-	uint32_t i, j;
+	uint32_t i;
+	uint32_t j;
 
 	pr_info("- writing inode %u\n", inode->inumber);
 	pr_info("writing data blocks to offset %u\n", blk_idx * KFS_BLK_SZ);
@@ -137,7 +143,7 @@ kfs_write_inode(FILE *out, FILE *fp, struct kfs_inode *inode, uint32_t blk_idx)
 
 	/* write direct blocks */
 	for (i = 0; i < KFS_DIRECT_BLK && kfs_read_block(fp, &blk); ++blk_idx, ++i) {
-		pr_info("write direct data block to offset %u\n", blk_idx * KFS_BLK_SZ);
+		pr_info("write direct block to offset %u\n", blk_idx * KFS_BLK_SZ);
 
 		blk.idx = blk_idx;
 		blk.cksum = kfs_checksum(&blk, sizeof(struct kfs_block));
@@ -157,11 +163,12 @@ kfs_write_inode(FILE *out, FILE *fp, struct kfs_inode *inode, uint32_t blk_idx)
 
 			/* fill indirect blocks */
 			for (j = 0; j < KFS_INDIRECT_BLK_CNT; ++j) {
-				if (!kfs_read_block(fp, &blk)) {
+
+				if (!kfs_read_block(fp, &blk))
 					break;
-				}
+
 				blk.idx = blk_idx++;
-				blk.cksum = kfs_checksum(&blk, sizeof(struct kfs_block));
+				blk.cksum = kfs_checksum(&blk, sizeof(blk));
 				iblock_idx.blks[iblock_idx.blk_cnt++] = blk.idx;
 
 				pr_info("writing indirect data block to offset %u\n", blk.idx * KFS_BLK_SZ);
@@ -181,7 +188,7 @@ kfs_write_inode(FILE *out, FILE *fp, struct kfs_inode *inode, uint32_t blk_idx)
 			return 0;
 		}
 	}
-	inode->cksum = kfs_checksum(inode, sizeof(struct kfs_inode) - sizeof(inode->cksum));
+	inode->cksum = kfs_checksum(inode, sizeof(inode) - sizeof(inode->cksum));
 
 	pr_info("writing inode to offset %u\n", inode->idx * KFS_BLK_SZ);
 	fseek(out, inode->idx * KFS_BLK_SZ, SEEK_SET);
@@ -202,10 +209,9 @@ kfs_write_files(FILE *out, char **files, size_t nb_files, size_t blkoff)
 
 	for (size_t i = 0; i < nb_files; ++i) {
 		FILE *fp = fopen(files[i], "r");
-		if (!fp) {
+
+		if (!fp)
 			err(1, "unable to open \"%s\"", files[i]);
-			return 0;
-		}
 
 		blk_idx = kfs_write_inode(out, fp, inodes[i], blk_idx);
 
@@ -264,6 +270,7 @@ int main(int argc, char **argv)
 		rom_name = rom_file;
 
 	FILE *rom = fopen(rom_file, "w");
+
 	if (!rom)
 		err(1, "error opening file %s for writing", rom_file);
 
