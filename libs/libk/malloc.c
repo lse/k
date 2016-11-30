10c0f0b4f28c7c4131417c1ee0590bc46727e979
Gabriel Laskar <gabriel@lse.epita.fr>
2016-11-30 14:55:34 +0100

    libk: add malloc debug messages
    Signed-off-by: Gabriel Laskar <gabriel@lse.epita.fr>

--- libs/libk/malloc.c
+++ libs/libk/malloc.c
@@ -1,12 +1,28 @@
 #include <kstd.h>
 #include <string.h>
 
+//#define DEBUG_MALLOC
+
+#ifdef DEBUG_MALLOC
+#include <stdio.h>
+#define pr_debug(fmt, ...) \
+	printf("[-] malloc: " fmt, ## __VA_ARGS__)
+#else
+#define pr_debug(fmt, ...) /* nothing */
+#endif
+
 void *malloc(size_t size)
 {
-	return sbrk(size);
+	void *ptr = sbrk(size);
+
+	pr_debug("malloc(%u) = %p\n", size, ptr);
+
+	return ptr;
 }
 
 void free(void *ptr)
 {
 	(void)ptr;
+
+	pr_debug("free(%p)\n", ptr);
 }
