600dd33e4eb7affd65d88a7be74fd6b78d6167c1
Gabriel Laskar <gabriel@lse.epita.fr>
2017-01-11 19:31:02 +0100

    libc: use linux's early printf
    This also introduce strnlen and puts.
    
    Signed-off-by: Gabriel Laskar <gabriel@lse.epita.fr>

--- libs/libc/printf.c
+++ libs/libc/printf.c
@@ -15,7 +15,15 @@
  *
  */
 
-#include "boot.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+static inline int isdigit(int c)
+{
+	return c >= '0' && c <= '9';
+}
 
 static int skip_atoi(const char **s)
 {
