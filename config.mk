480ddf8133720bb8894bf8d0cae889a63a68b687
Gabriel Laskar <gabriel@lse.epita.fr>
2017-01-26 01:29:38 +0100

    build: more debug flags
    Signed-off-by: Gabriel Laskar <gabriel@lse.epita.fr>

--- config.mk
+++ config.mk
@@ -2,9 +2,11 @@ CFLAGS	= -std=gnu99 -Os -Wall -Wextra -nostdinc -fno-builtin -ffreestanding \
 	  -m32 -fno-asynchronous-unwind-tables -fno-common -static
 # SSP causes compilation problems on Ubuntu
 CFLAGS	+= -fno-stack-protector
-#CFLAGS += -g3
+#K_EXTRA_CFLAGS = -g3
+#CFLAGS += -ffunction-sections -fdata-sections
 CPPFLAGS += -I$(shell $(CC) -m32 --print-file-name=include)
 
 ASFLAGS = -m32
 LDFLAGS	= -nostdlib -m32 -Wl,--build-id=none -nostartfiles
+#LDFLAGS += -Wl,--gc-sections -Wl,--print-gc-sections
 ARFLAGS = src
