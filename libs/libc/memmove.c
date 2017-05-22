567f2da3d0f515e7c9fafc2e1ee103b82a5db7e4
Antoine Damhet <antoine.damhet@lse.epita.fr>
2017-05-22 16:51:02 +0200

    libc: extend bundled libc
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>
    Acked-by: Gabriel Laskar <gabriel@lse.epita.fr>

---
+++ libs/libc/memmove.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+	char *d = dest;
+	const char *s = src;
+
+	if (s < d && s + n > d) {
+		while (n-- > 0)
+			d[n - 1] = s[n - 1];
+		return dest;
+	}
+
+	return memcpy(dest, src, n);
+}
