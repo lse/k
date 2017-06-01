3d3f2e813fc67abbbe6e0a3bad1fdb91f140acdd
Antoine Damhet <antoine.damhet@lse.epita.fr>
2017-06-01 19:54:05 +0200

    libc: fix: strncasecmp is now exported
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>
    Reviewed-by: Gabriel Laskar <gabriel@lse.epita.fr>

--- libs/libc/include/string.h
+++ libs/libc/include/string.h
@@ -32,6 +32,7 @@ void *memcpy(void *dest, const void *src, size_t n);
 void *memmove(void *dest, const void *src, size_t n);
 void *memset(void *s, int c, size_t n);
 int strcasecmp(const char *s1, const char *s2);
+int strncasecmp(const char *s1, const char *s2, size_t n);
 char *strcat(char *dest, const char *src);
 int strcmp(const char *s1, const char *s2);
 char *strcpy(char *dest, const char *src);
