6c49e1a75dd4355f553179b7749b0a595b44886a
Antoine Damhet <antoine.damhet@lse.epita.fr>
2017-10-31 10:36:56 +0100

    k: compiler.h: introduce align_up macro
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

--- k/include/k/compiler.h
+++ k/include/k/compiler.h
@@ -3,5 +3,6 @@
 
 #define array_size(arr)	(sizeof(arr) / sizeof(*arr))
 #define __packed __attribute__((__packed__))
+#define align_up(addr, bytes) (((addr) + (bytes) - 1) & ~((bytes) - 1))
 
 #endif
