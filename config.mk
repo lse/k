f9f2732b5b6bd90e47d4c9aa5a5a3bbd8450aa6f
Antoine Damhet <antoine.damhet@lse.epita.fr>
2017-12-15 16:39:59 +0100

    config.mk: ld: garbage collect unused functions/datas by default
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

--- config.mk
+++ config.mk
@@ -3,12 +3,15 @@ CFLAGS	= -std=gnu99 -Os -Wall -Wextra -nostdinc -fno-builtin -ffreestanding \
 # SSP causes compilation problems on Ubuntu
 CFLAGS	+= -fno-stack-protector
 #K_EXTRA_CFLAGS = -g3
-#CFLAGS += -ffunction-sections -fdata-sections
+# Place each function or data item into a separate section
+CFLAGS += -ffunction-sections -fdata-sections
 CPPFLAGS += -I$(shell $(CC) -m32 --print-file-name=include)
 
 ASFLAGS = -m32
 LDFLAGS = -nostdlib -m32 -Wl,--build-id=none -nostartfiles -static
-#LDFLAGS += -Wl,--gc-sections -Wl,--print-gc-sections
+# Detect and remove unused sections while linking the objects
+LDFLAGS += -Wl,--gc-sections
+#LDFLAGS += -Wl,--print-gc-sections
 ARFLAGS = src
 INSTALL = install -C -D
 INSTALL_ROOT ?= iso
