1bc0cfef9f66e8c0680ee2662fb1b84afb8a37b5
Gabriel Laskar <gabriel@lse.epita.fr>
2018-04-04 21:15:49 +0200

    build: add a comment for self-corruption tests in malloc
    Suggested-by: Antoine Damhet <xdbob@lse.epita.fr>
    Signed-off-by: Gabriel Laskar <gabriel@lse.epita.fr>

--- config.mk
+++ config.mk
@@ -6,6 +6,8 @@ CFLAGS	+= -fno-stack-protector
 # Place each function or data item into a separate section
 CFLAGS += -ffunction-sections -fdata-sections
 CPPFLAGS += -I$(shell $(CC) -m32 --print-file-name=include)
+# enable dlmalloc self-corruption tests.
+#CPPFLAGS += -DDEBUG=1 -DFOOTERS=1
 
 ASFLAGS = -m32
 LDFLAGS = -nostdlib -m32 -Wl,--build-id=none -nostartfiles -static
