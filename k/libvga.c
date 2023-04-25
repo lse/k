lse/k

commit 29e29bef64a4bf9dea9f43eef47ba196bed722dd

k/libvga: mark unreachable impossible code path
Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

k/libvga.c
@@ -23,8 +23,9 @@
 */
 #include <k/compiler.h>
 
-#include "libvga.h"
 #include "io.h"
+#include "libvga.h"
+#include "panic.h"
 
 /*
 ** Use to save the VGA plane 2, which contains the text font,
@@ -227,7 +228,7 @@ char *libvga_get_framebuffer(void)
 	case 3:
 		return (char *)0xB8000;
 	}
-	return (char *)0;
+	__builtin_unreachable();
 }
 
 void libvga_switch_mode13h(void)

commit fe72dc07aeb1a56db9199ea97f1d6f75ef9a9473

k/libvga: remove useless `panic.h` include
Was wrongfully introduced by 29e29bef64a4bf9dea9f43eef47ba196bed722dd

Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

k/libvga.c
@@ -25,7 +25,6 @@
 
 #include "io.h"
 #include "libvga.h"
-#include "panic.h"
 
 /*
 ** Use to save the VGA plane 2, which contains the text font,